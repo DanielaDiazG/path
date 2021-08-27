af82bf2cb9f98a51feced31dc0808ef3881d7442
DanielaDiazGomez <ddiaz@ayenda.co>
2021-08-27 15:57:37 -0500

    reto 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css"
+import logo from "./icon-react.svg"
+
+function App() {
+    return(
+        <div className="app">
+            <h2 >Hola React</h2>
+            <img src={logo} alt="react" className="image"></img>
+        </div>
+    )
+}
+
+export default App;
